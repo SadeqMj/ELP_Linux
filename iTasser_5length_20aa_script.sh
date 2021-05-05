208d22afd0b28f7f1214dd4021e9846bd03fd826
SadeqMj <83470035+SadeqMj@users.noreply.github.com>
2021-05-05 11:56:39 -0400

    Add files via upload

---
+++ iTasser_5length_20aa_script.sh
@@ -0,0 +1,264 @@
+set -e
+
+#make directories for iTasser Fasta files VPGPG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGPG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGPG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGPG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGPG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGPG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGPG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGPG30/Output/VPGPG30
+
+
+
+
+
+#make directories for iTasser Fasta files VPGAG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGAG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGAG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGAG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGAG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGAG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGAG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGAG30/Output/VPGAG30
+
+
+
+
+
+#make directories for iTasser Fasta files VPGCG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGCG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGCG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGCG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGCG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGCG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGCG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGCG30/Output/VPGCG30
+
+
+
+
+
+#make directories for iTasser Fasta files VPGDG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGDG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGDG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGDG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGDG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGDG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGDG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGDG30/Output/VPGDG30
+
+
+
+
+
+#make directories for iTasser Fasta files VPGEG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGEG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGEG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGEG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGEG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGEG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGEG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGEG30/Output/VPGEG30
+
+
+
+
+#make directories for iTasser Fasta files VPGFG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGFG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGFG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGFG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGFG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGFG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGFG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGFG30/Output/VPGFG30
+
+
+
+
+#make directories for iTasser Fasta files VPGGG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGGG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGGG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGGG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGGG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGGG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGGG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGGG30/Output/VPGGG30
+
+
+
+
+#make directories for iTasser Fasta files VPGHG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGHG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGHG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGHG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGHG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGHG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGHG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGHG30/Output/VPGHG30
+
+
+
+
+#make directories for iTasser Fasta files VPGIG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGIG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGIG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGIG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGIG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGIG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGIG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGIG30/Output/VPGIG30
+
+
+
+
+#make directories for iTasser Fasta files VPGKG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGKG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGKG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGKG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGKG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGKG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGKG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGKG30/Output/VPGKG30
+
+
+
+
+#make directories for iTasser Fasta files VPGLG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGLG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGLG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGLG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGLG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGLG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGEG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGLG30/Output/VPGLG30
+
+
+
+
+#make directories for iTasser Fasta files VPGMG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGMG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGMG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGMG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGMG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGMG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGMG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGMG30/Output/VPGMG30
+
+
+
+
+#make directories for iTasser Fasta files VPGNG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGNG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGNG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGNG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGNG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGNG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGNG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGNG30/Output/VPGNG30
+
+
+
+
+#make directories for iTasser Fasta files VPGQG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGQG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGQG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGQG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGQG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGQG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGQG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGQG30/Output/VPGQG30
+
+
+
+
+#make directories for iTasser Fasta files VPGRG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGRG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGRG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGRG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGRG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGRG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGRG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGRG30/Output/VPGRG30
+
+
+
+
+#make directories for iTasser Fasta files VPGSG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGSG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGSG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGSG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGSG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGSG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGSG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGSG30/Output/VPGSG30
+
+
+
+
+#make directories for iTasser Fasta files VPGTG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGTG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGTG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGTG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGTG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGTG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGTG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGTG30/Output/VPGTG30
+
+
+
+
+#make directories for iTasser Fasta files VPGVG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGVG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGVG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGVG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGVG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGVG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGVG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGVG30/Output/VPGVG30
+
+
+
+
+#make directories for iTasser Fasta files VPGWG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGWG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGWG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGWG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGWG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGWG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGWG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGWG30/Output/VPGWG30
+
+
+
+
+#make directories for iTasser Fasta files VPGYG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGYG30
+mkdir /home/general/Programs/I-TASSER5.1/Working/VPGYG30/Output
+
+#Copy file from repository to working directory
+cp /home/general/Programs/I-TASSER5.1/Working/Fasta_Repository/VPGYG30.fasta /home/general/Programs/I-TASSER5.1/Working/VPGYG30/seq.fasta
+
+#Run iTasser
+/home/general/Programs/I-TASSER5.1/I-TASSERmod/runI-TASSER.pl -pkgdir /home/general/Programs/I-TASSER5.1 -libdir /home/general/Programs/I-TASSER5.1/libfiles -LBS true -EC true -GO true -seqname VPGYG30 -datadir /home/general/Programs/I-TASSER5.1/Working/VPGYG30/seq.fasta -java_home /usr -light true -hours 6 -outdir /home/general/Programs/I-TASSER5.1/Working/VPGYG30/Output/VPGYG30
+
+
